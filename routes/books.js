7e2b75ba086fc83f3855a65ec42f6ef50cd03406
Carmel Meelind <cmeelind@tktk.ee>
2023-10-31 13:22:23 +0200

    Week 6 files

---
+++ routes/books.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data/books.json');
+
+router.get('/', (req, res) => {
+    res.status(200).json(data);
+});
+
+router.get('/:bookId', (req, res) => {
+
+    const requestedBookId = req.params.bookId;
+
+    const book = data.books.filter(bookInData => {
+    
+        if(bookInData.id.toString() === requestedBookId) {
+            return bookInData;
+        }
+        //if(bookInData.id === erq.params.bookId){
+            //return bookInData;
+        //}
+    });
+
+    res.status(200).json(book);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const port = 3000;
+const booksRouter = require('./routes/books');
+const countriesRouter = require('./routes/countries');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+
+    //res.send('<h1>Hello from app get /</h1>');
+    //res.sendStatus(200);  
+    //res.status(500).json({message: "Internal error. Try again later."});
+    //res.download('server.js');
+    res.render('index');
+});
+
+app.use('/books', booksRouter);
+
+app.use('/countries', countriesRouter);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port${port}`);
+});
